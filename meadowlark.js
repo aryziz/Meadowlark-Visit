ab119ddb079d2ef7b8943f3ce12d1fda3490bfa4
Aryan Aziz Mathiesen <aryanam@uia.no>
2024-06-10 19:29:17 +0200

    Added newsletter

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,7 @@ const handlers = require('./lib/handlers');
 const weatherMiddleware = require('./lib/middleware/weather');
 
 const expressHandlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -25,6 +26,8 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 app.use(weatherMiddleware);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', handlers.home);
 
@@ -32,8 +35,12 @@ app.get('/about', handlers.about);
 
 // Showing headers from a request
 app.get('/headers', handlers.showHeader);
-
 app.get('/section-test', handlers.sectionTest);
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
 
 
 if (require.main == module) {
